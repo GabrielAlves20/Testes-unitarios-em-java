ec5aa0ea91919c6e258043b436ceec84805d01a0
Gabriel <gabriel12425@gmail.com>
2025-10-06 20:19:42 -0300

    Primeiro teste unitario

---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,43 @@
+import org.example.Calculadora;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+    @Test
+    public void deveSomarDoisValores(){
+        Calculadora calculadora = new Calculadora();
+        double a = 10;
+        double b = 20;
+        double resultado = calculadora.somar(a, b);
+
+        Assertions.assertEquals(30, resultado);
+    }
+    @Test
+    public void deveMultiplicarDoisValores(){
+        Calculadora calculadora = new Calculadora();
+        double a = 10;
+        double b = 20;
+        double resultado = calculadora.multiplicar(a, b);
+
+        Assertions.assertEquals(200, resultado);
+    }
+    @Test
+    public void deveDividirDoisValores(){
+        Calculadora calculadora = new Calculadora();
+        double a = 10;
+        double b = 20;
+        double resultado = calculadora.dividir(a, b);
+
+        Assertions.assertEquals(0.5, resultado);
+    }
+    @Test
+    public void deveSubtrairDoisValores(){
+        Calculadora calculadora = new Calculadora();
+        double a = 10;
+        double b = 20;
+        double resultado = calculadora.subtrair(a, b);
+
+        Assertions.assertEquals(-10, resultado);
+    }
+
+}
